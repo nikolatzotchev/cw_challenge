57cd3a1789ec0f3d666e3a29c57800da2e11c7fa
nikolatzotchev <ntzotchev@gmail.com>
2023-07-30 10:39:49 +0200

    add getter and setters to task

--- src/main/java/com/craftworks/challenge/models/Task.java
+++ src/main/java/com/craftworks/challenge/models/Task.java
@@ -48,4 +48,76 @@ public class Task {
   public Task() {
 
   }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public Date getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedAt(Date createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  public Date getUpdatedAt() {
+    return updatedAt;
+  }
+
+  public void setUpdatedAt(Date updatedAt) {
+    this.updatedAt = updatedAt;
+  }
+
+  public Date getDueDate() {
+    return dueDate;
+  }
+
+  public void setDueDate(Date dueDate) {
+    this.dueDate = dueDate;
+  }
+
+  public Date getResolvedAt() {
+    return resolvedAt;
+  }
+
+  public void setResolvedAt(Date resolvedAt) {
+    this.resolvedAt = resolvedAt;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public Priority getPriority() {
+    return priority;
+  }
+
+  public void setPriority(Priority priority) {
+    this.priority = priority;
+  }
+
+  public Status getStatus() {
+    return status;
+  }
+
+  public void setStatus(Status status) {
+    this.status = status;
+  }
 }
